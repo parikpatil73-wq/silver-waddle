b35e0db624f1704c353d888770ad638e543009d6
parikpatil73-wq <parikpatil73@gmail.com>
2025-10-07 00:57:45 +0400

    Fix authenticator.login() API call for streamlit-authenticator 0.4.2

--- stock_analysis_app.py
+++ stock_analysis_app.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login(location='main')
 
 if authentication_status:
     st.success(f"Welcome {name}!")
